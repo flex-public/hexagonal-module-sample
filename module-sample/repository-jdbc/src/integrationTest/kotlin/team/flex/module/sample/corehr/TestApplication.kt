856d00bc22fad0509dba1b868ba5e5b1b541555f
구경일 <62415185+flex-gyeongil@users.noreply.github.com>
2025-06-03 13:47:34 +0900

    :zap: repository test의 spring context가 재사용 가능하게 조정 (#2)

--- module-sample/repository-jdbc/src/integrationTest/kotlin/team/flex/module/sample/corehr/TestApplication.kt
+++ module-sample/repository-jdbc/src/integrationTest/kotlin/team/flex/module/sample/corehr/TestApplication.kt
@@ -6,8 +6,17 @@ package team.flex.module.sample.corehr
 
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Import
+import team.flex.module.sample.corehr.company.department.repository.DepartmentRepositoryAutoConfiguration
+import team.flex.module.sample.corehr.company.repository.CompanyRepositoryAutoConfiguration
+import team.flex.module.sample.corehr.employee.repository.EmployeeRepositoryAutoConfiguration
 import java.util.TimeZone
 
+@Import(
+    EmployeeRepositoryAutoConfiguration::class,
+    CompanyRepositoryAutoConfiguration::class,
+    DepartmentRepositoryAutoConfiguration::class,
+)
 @EnableAutoConfiguration
 @SpringBootConfiguration
 class TestApplication {
